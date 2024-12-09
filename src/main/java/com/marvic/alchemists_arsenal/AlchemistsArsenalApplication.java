b4eea660e54dc5a210a637c097652aca0eefddb2
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-09 13:12:19 -0300

    commit inicial

---
+++ src/main/java/com/marvic/alchemists_arsenal/AlchemistsArsenalApplication.java
@@ -0,0 +1,13 @@
+package com.marvic.alchemists_arsenal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlchemistsArsenalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlchemistsArsenalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marvic/alchemists_arsenal/AlchemistsArsenalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvic.alchemists_arsenal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlchemistsArsenalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
