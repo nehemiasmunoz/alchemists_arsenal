0d5e472a0a5d7c1e2c754c984aa3d143fbc50318
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-10 10:07:56 -0300

    add: repository, dependencias agregando repository de paciente dependencia postgres dependecia jpa

---
+++ src/main/java/com/marvic/alchemists_arsenal/IPacientRepository.java
@@ -0,0 +1,8 @@
+package com.marvic.alchemists_arsenal;
+
+import com.marvic.alchemists_arsenal.model.Pacient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPacientRepository extends JpaRepository<Pacient, Long>{}
