623966d21917648b5f5e769ab49354b98fa9b335
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-10 10:15:51 -0300

    upd: prueba

--- src/main/java/com/marvic/alchemists_arsenal/AlchemistsArsenalApplication.java
+++ src/main/java/com/marvic/alchemists_arsenal/AlchemistsArsenalApplication.java
@@ -1,13 +1,28 @@
 package com.marvic.alchemists_arsenal;
 
+import com.marvic.alchemists_arsenal.model.Pacient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AlchemistsArsenalApplication {
-
+	private Logger log = LoggerFactory.getLogger(AlchemistsArsenalApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(AlchemistsArsenalApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(IPacientRepository pacientRepository){
+		return (args) -> {
+
+			Pacient nehemias = Pacient.make("Nehemias", "mmasculino");
+			Pacient ambar = Pacient.make("Ambar", "feminino");
+			log.info("Paciente 1: {}",pacientRepository.save(nehemias));
+			log.info("Paciente 2: {}",pacientRepository.save(ambar));
+		};
+	}
 }
