6d69f46605d8abb93f1681677bb7eeca165df843
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-13 11:36:38 -0300

    upd: uso de interfaz en remplado de clase Nuevo endpoint para crear paciente. Uso de DTOs en remplazo de entidades

--- src/main/java/com/marvic/alchemists_arsenal/restController/PacientRestController.java
+++ src/main/java/com/marvic/alchemists_arsenal/restController/PacientRestController.java
@@ -1,26 +1,33 @@
 package com.marvic.alchemists_arsenal.restController;
 
-import com.marvic.alchemists_arsenal.model.Pacient;
+import com.marvic.alchemists_arsenal.dtos.pacient.PacientDTO;
+import com.marvic.alchemists_arsenal.interfaces.IPacientService;
+import com.marvic.alchemists_arsenal.model.pacient.Pacient;
 import com.marvic.alchemists_arsenal.services.PacientService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/pacients")
 public class PacientRestController {
-    private PacientService pacientService;
+    private final IPacientService pacientService;
 
     PacientRestController(PacientService service){
         this.pacientService = service;
     }
 
 
-    @GetMapping("/pacients")
-    public ResponseEntity<List<Pacient>> getAllPacient(){
-        return ResponseEntity.ok().body(pacientService.getAllPacients());
+    @GetMapping("/")
+    public ResponseEntity<List<PacientDTO>> getAllPacient(){
+        List<PacientDTO> pacients = pacientService.getPacients();
+        return ResponseEntity.ok().body(pacients);
+    }
+
+    @PostMapping("/save/")
+    public ResponseEntity<PacientDTO> savePacient(@RequestBody PacientDTO pacient){
+        PacientDTO pacientDTO = pacientService.addPacient(pacient);
+        return ResponseEntity.ok().body(pacientDTO);
     }
 }
